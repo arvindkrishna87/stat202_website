b5b5c3e657d2feb1d3c1debf76e7cad6890c14f9
dsass1 <dsass1@comcast.net>
2022-12-31 09:12:09 -0600

    Initial updates for winter quarter

--- documents/clean_agenda.R
+++ documents/clean_agenda.R
@@ -27,22 +27,41 @@ main <- tibble(Date = seq(class_start,
   mutate(day = wday(Date)) %>% 
   filter(day %in% c(2, 4, 6) |  #mon, wed, fri
            Date == class_start) %>% #NU Tue
-  select(Date)
+  select(Date) %>% 
+  filter(!Date %in% c(add_dates$Date))
 # must equal length of course-schedule.csv (27 days)
 
 #match dates with topic covered
 agenda_classes <- cbind(main, schedule)
 
-#add reading week and final
-agenda <- bind_rows(agenda_classes, add_dates)
+#add holidays, reading week and final
+agenda <- bind_rows(agenda_classes, add_dates) %>% 
+  arrange(Date)
+
 
 #set blanks as empty instead of NA
 agenda[is.na(agenda)] <- "" 
 
 #add due dates
 agenda <- agenda %>% 
-  mutate(due_ymd = if_else(wday(Date) == 6, Date+2,Date +1),
+  mutate(due_ymd = case_when(wday(Date) == 6 ~ Date + 2,
+                             wday(Date) == 3 ~ Date + 0,
+                             TRUE ~ Date + 1),
+           #if_else(wday(Date) %in% c(3,6), Date+2,Date +1),
          due = format(due_ymd, "%a %b %d"))
+
+
+#adjust due date for MLK in Winter Quarter
+mlk_row <- which(stringr::str_detect(agenda$Topic, "MLK"))
+
+if(length(mlk_row) != 0){
+  agenda$submit_icon[mlk_row] <- agenda$submit_icon[mlk_row-1]
+  agenda$submit_link[mlk_row] <- agenda$submit_link[mlk_row-1]
+  agenda$tutorial_icon[mlk_row] <- agenda$tutorial_icon[mlk_row-1]
+  agenda$tutorial_link[mlk_row] <- agenda$tutorial_link[mlk_row-1]
+  agenda$due_ymd[mlk_row-1] <- agenda$due_ymd[mlk_row]
+  agenda$due[mlk_row-1] <- agenda$due[mlk_row]
+}
 
 # format all data
 agenda_final <- agenda %>% 
