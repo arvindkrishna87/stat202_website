6d494bbc361417265ac36b9a5c1afb9801dd9c0f
dsass1 <dsass1@comcast.net>
2023-09-25 08:34:23 -0500

    update lecture videos

--- documents/clean_agenda.R
+++ documents/clean_agenda.R
@@ -82,10 +82,12 @@ agenda_final <- agenda %>%
          Activity = paste0(activity_icon, activity_link),
          submit_act = paste0(submit_icon, submit_link),
          Tutorial = paste0(tutorial_icon, tutorial_link),
+         Lectures = paste0(lecture_icon, lecture_link),
          Solutions = paste0(solution_icon, solution_link)) %>% 
   select(-c(ReadingLink, slide_icon, slide_link, activity_icon, 
          activity_link, submit_icon, submit_link, tutorial_icon, 
-         tutorial_link, solution_icon, solution_link))
+         tutorial_link, lecture_icon, lecture_link, 
+         solution_icon, solution_link))
 
 agenda_final$reading_hw <- c(agenda_final$Reading[-1], "")
 
